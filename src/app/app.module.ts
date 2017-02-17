85815af75513b85360b126c6b063296f51ae02ce
cyberbeast <sandeshgade@gmail.com>
2017-02-16 23:56:15 -0500

    Project Scaffolding and Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {InputTextModule, ButtonModule, ConfirmDialogModule,ConfirmationService} from 'primeng/primeng';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InputTextModule,
+    ButtonModule,
+    ConfirmDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
