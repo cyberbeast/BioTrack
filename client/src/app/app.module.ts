39f21e7df3d62bc266c3d89ec29643f4d28482ae
Sandesh Gade <sandeshgade@gmail.com>
2017-05-23 18:57:38 -0400

    Updated app configurations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,26 @@
+// import { MaterializeModule } from 'angular2-materialize';
+import { MaterializeModule } from 'ng2-materialize';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavHeaderComponent } from './navbar/sidenav-header/sidenav-header.component';
+import { SidenavSectionsComponent } from './navbar/sidenav-sections/sidenav-sections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavHeaderComponent,
+    SidenavSectionsComponent
   ],
   imports: [
+    MaterializeModule.forRoot(),
+    BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule
