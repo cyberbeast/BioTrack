4ec732ea8a3c4da42feac7561d13b1f2cac71a6b
Sandesh Gade <sandeshgade@gmail.com>
2017-06-02 18:29:50 -0400

    update store implementation to handle current mode

--- client/src/app/common/stores/selectedMode.store.ts
+++ client/src/app/common/stores/selectedMode.store.ts
@@ -2,7 +2,7 @@ import { ActionReducer, Action } from '@ngrx/store';
 
 export const SET_MODE = 'SET_MODE';
 
-export function selectedModeReducer(state: string = "suppliers", action: Action) {
+export function selectedModeReducer(state: string = "dashboard", action: Action) {
 	switch (action.type) {
 		case SET_MODE:
 			return action.payload;
