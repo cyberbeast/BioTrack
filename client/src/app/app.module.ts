16a78be282809cca62b9828f06d95452c976be17
Sandesh Gade <sandeshgade@gmail.com>
2017-06-02 18:29:25 -0400

    updated bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -35,8 +35,6 @@ import { selectedModeReducer } from './common/stores/selectedMode.store';
 // DECLARATIONS
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SidenavHeaderComponent } from './navbar/sidenav-header/sidenav-header.component';
-import { SidenavSectionsComponent } from './navbar/sidenav-sections/sidenav-sections.component';
 import { SuppliersComponent } from './suppliers/suppliers.component';
 
 // ROUTING DECLARATIONS
@@ -45,6 +43,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { SuppliersMasterComponent } from './suppliers/suppliers-master/suppliers-master.component';
 import { SuppliersDetailComponent } from './suppliers/suppliers-detail/suppliers-detail.component';
 import { RedComponentComponent } from "./suppliers/suppliers-detail/red-comp.component";
+import { NavbarSectionsComponent } from './navbar/navbar-sections/navbar-sections.component';
 
 
 
@@ -52,13 +51,12 @@ import { RedComponentComponent } from "./suppliers/suppliers-detail/red-comp.com
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidenavHeaderComponent,
-    SidenavSectionsComponent,
     SuppliersComponent,
     DashboardComponent,
     SuppliersMasterComponent,
     SuppliersDetailComponent,
-    RedComponentComponent
+    RedComponentComponent,
+    NavbarSectionsComponent
   ],
   imports: [
     MaterializeModule.forRoot(),
