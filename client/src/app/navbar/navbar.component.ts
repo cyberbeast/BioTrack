ffef2fd376bfc5ed7b096343acb506583535c9e5
Sandesh Gade <sandeshgade@gmail.com>
2017-06-02 09:22:48 -0400

    navbar updates

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import * as screenfull from 'screenfull';
+import { Observable } from "rxjs/Observable";
+import { Store } from '@ngrx/store';
+import { ModesService } from '../common/services/modes.service';
+import { AppStore } from '../common/models/appstore.model';
+import { Mode } from '../common/models/mode.model';
+
 
 @Component({
   selector: 'biotrack-navbar',
@@ -8,15 +14,34 @@ import * as screenfull from 'screenfull';
 })
 export class NavbarComponent implements OnInit {
 
+  selectedMode: Observable<string>;
+
   fullscreen() {
     if (screenfull.enabled) {
             screenfull.toggle();
     }
   }
 
-  constructor() { }
+  tempMode: string = 'a';
 
-  ngOnInit() {
+  isActive(mode) {
+    if (mode ===  this.tempMode){
+      return true;
+    }
+    else {
+      return false;
+    }
   }
+
+  constructor(
+    private modesService: ModesService,
+    private store: Store<AppStore>
+  ) {
+    this.selectedMode = store.select('selectedMode');
+    this.selectedMode.subscribe(v => this.tempMode = v);
+
+  }
+
+  ngOnInit() { }
 
 }
