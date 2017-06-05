933b0aaa1581da3d317d4f2a0da635b57c05ad85
Sandesh Gade <sandeshgade@gmail.com>
2017-06-05 15:52:49 -0400

    update subjects by suppliers tab view

--- client/src/app/suppliers/suppliers-detail/suppliers-detail.component.ts
+++ client/src/app/suppliers/suppliers-detail/suppliers-detail.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SuppliersDetailComponent implements OnInit {
   @Input() selectedSupplier;
+  @Input() subjectsBySelectedSupplier;
 
   name: string;
   description: string;
--- client/src/app/suppliers/suppliers-detail/suppliers-details-subjects-tab/suppliers-details-subjects-tab.component.ts
+++ client/src/app/suppliers/suppliers-detail/suppliers-details-subjects-tab/suppliers-details-subjects-tab.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SuppliersDetailsSubjectsTabComponent implements OnInit {
 
-  @Input() id;
-  
+  @Input() subjectsBySelectedSupplier;
+
   constructor() { }
 
   ngOnInit() {
